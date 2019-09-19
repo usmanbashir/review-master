a0e889b64e6cd0ebc68d56e7612d3e3854109472
Usman Bashir <me@usmanbashir.com>
2019-09-19 19:42:51 +0300

    The Big Bang!

---
+++ README.md
@@ -0,0 +1,3 @@
+# Review Master
+
+An easy-to-use, simple service that offers reviews of restaurants around you.
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Review Master</h1>
+        <p>
+          An easy-to-use, simple service that offers reviews of restaurants around you.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
