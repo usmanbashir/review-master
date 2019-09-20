2f054a0bf3258bc2029c86f2598a3eac44d483f8
Usman Bashir <me@usmanbashir.com>
2019-09-20 23:59:06 +0300

    Refactor getting user location into a custom hook

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import useUserLocation from './hooks/useUserLocation';
 
 function App() {
-  const [userLocation, setUserLocation] = useState({latitude: null, longitude: null});
-
-  // Get the users Geolocation when the app loads for the first time.
-  useEffect(() => {
-    // Only search for the location if the user's browser supports it.
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setUserLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
-      });
-    } else {
-      alert('Sorry, but your browser does not supports getting your Geolocation.')
-    }
-  }, []);
+  const userLocation = useUserLocation();
 
   return (
     <div className="App">
       <Header />
       {/* Show a message while waiting for the user to allow access to their location. */}
-      {userLocation.latitude === null &&
+      {!userLocation.found &&
         <div className="notice">Waiting for your location...</div>
       }
     </div>
---
+++ src/hooks/useUserLocation.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+const useUserLocation = () => {
+  const [userLocation, setUserLocation] = useState({lat: null, lng: null, found: false});
+
+  // Get the users Geolocation when the app loads for the first time.
+  useEffect(() => {
+    // Only search for the location if the user's browser supports it.
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          found: true
+        });
+      });
+    } else {
+      alert('Sorry, but your browser does not supports getting your Geolocation.')
+    }
+  }, []);
+
+  return userLocation;
+}
+
+export default useUserLocation;
