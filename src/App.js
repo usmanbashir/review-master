57e00989e667040713739e648d12d6647715b71d
Usman Bashir <me@usmanbashir.com>
2019-09-20 17:58:32 +0300

    Get the users geolocation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [userLocation, setUserLocation] = useState({latitude: null, longitude: null});
+
+  // Get the users Geolocation when the app loads for the first time.
+  useEffect(() => {
+    // Only search for the location if the user's browser supports it.
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setUserLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      });
+    } else {
+      alert('Sorry, but your browser does not supports getting your Geolocation.')
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +26,10 @@ function App() {
         <p>
           An easy-to-use, simple service that offers reviews of restaurants around you.
         </p>
+        {/* Show a message while waiting for the user to allow access to their location. */}
+        {userLocation.latitude === null &&
+          <strong>Waiting for your location...</strong>
+        }
       </header>
     </div>
   );
